e2baf5a1d5b68f22b11642b1412ebf14ef17338e
atomicules <base@atomicules.co.uk>
2013-09-03 13:07:23 +0100

    Add in EV menu, doesn't function yet though
    - Add in free memory bits, also missed some for the other menus

--- exposurses.c
+++ exposurses.c
@@ -8,6 +8,33 @@ http://tldp.org/HOWTO/NCURSES-Programming-HOWTO/intro.html */
 /* Learning notes - This is a macro that is expanded (text substitution) before compiling */
 #define ARRAY_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
+char *exposure_array[] = {
+	"-6",		
+	"-5",		
+	"-4",		
+	"-3",		
+	"-2",		
+	"-1",		
+	"0",		
+	"1",		
+	"2",		
+	"3",		
+	"4",		
+	"5",		
+	"6",		
+	"7",		
+	"8",		
+	"9",		
+	"10",		
+	"11",		
+	"12",		
+	"13",		
+	"14",		
+	"15",		
+	"16",		
+	NULL
+};
+
 char *iso_array[] = {
 	"50",
 	"100",
@@ -46,12 +73,15 @@ char *aperture_array[] = {
 	NULL
 };
 
+ITEM **exposure_items;
 ITEM **iso_items;
 ITEM **shutter_items;
 ITEM **aperture_items;
+MENU *exposure_menu;
 MENU *iso_menu;
 MENU *shutter_menu;
 MENU *aperture_menu;
+WINDOW *exposure_win;
 WINDOW *iso_win;
 WINDOW *shutter_win;
 WINDOW *aperture_win;
@@ -65,6 +95,7 @@ double aperture(int exposure, double shutter);
 int nearest_match(double x, int menu);
 double fraction_to_double(char *fraction);
 /* No one will ever need more than 9 bytes! */
+char exposure_sel[9] = "";
 char iso_sel[9] = "";
 char shutter_sel[9] = "";
 char aperture_sel[9] = "";
@@ -74,9 +105,11 @@ int main() {
 	int c;
 	MENU **menu;
 	WINDOW **win;
-	int n_iso, i;
-	int n_shutter, j;
-	int n_aperture, k;
+	int i;
+	int n_exposure;
+	int n_iso;
+	int n_shutter;
+	int n_aperture;
 	int menu_sel_last;
 	selection_counter = 0;
 
@@ -90,9 +123,16 @@ int main() {
 	init_pair(2, COLOR_CYAN, COLOR_BLACK);
 
 	/* Create items */
+	/* Lot's of repitition here. Surely can be wrapped in a function */
+	n_exposure = ARRAY_SIZE(exposure_array);
 	n_iso = ARRAY_SIZE(iso_array);
 	n_shutter = ARRAY_SIZE(shutter_array);
 	n_aperture = ARRAY_SIZE(aperture_array);
+	exposure_items = (ITEM **)calloc(n_exposure, sizeof(ITEM *));
+	for(i = 0; i < n_exposure; ++i) {
+		exposure_items[i] = new_item(exposure_array[i], exposure_array[i]);
+		set_item_userptr(exposure_items[i], selection);
+	}
 	iso_items = (ITEM **)calloc(n_iso, sizeof(ITEM *));
 	for(i = 0; i < n_iso; ++i) {
 		iso_items[i] = new_item(iso_array[i], iso_array[i]);
@@ -110,55 +150,69 @@ int main() {
 	}
 
 	/* Create menu */
+	exposure_menu = new_menu((ITEM **)exposure_items);
 	iso_menu = new_menu((ITEM **)iso_items);
 	shutter_menu = new_menu((ITEM **)shutter_items);
 	aperture_menu = new_menu((ITEM **)aperture_items);
 
 	/* Create the window to be associated with the menu */
-	iso_win = newwin(10, 40, 4, 4);
+	exposure_win = newwin(10, 40, 4, 4);
+	keypad(exposure_win, TRUE);
+	iso_win = newwin(10, 40, 4, 45);
 	keypad(iso_win, TRUE);
-	shutter_win = newwin(10, 40, 4, 45);
+	shutter_win = newwin(10, 40, 4, 86);
 	keypad(shutter_win, TRUE);
-	aperture_win = newwin(10, 40, 4, 86);
+	aperture_win = newwin(10, 40, 4, 127);
 	keypad(aperture_win, TRUE);
 
 	/* Set main window and sub window */
+	set_menu_win(exposure_menu, exposure_win);
+	set_menu_sub(exposure_menu, derwin(exposure_win, 6, 38, 3, 1));
 	set_menu_win(iso_menu, iso_win);
 	set_menu_sub(iso_menu, derwin(iso_win, 6, 38, 3, 1));
 	set_menu_win(shutter_menu, shutter_win);
 	set_menu_sub(shutter_menu, derwin(shutter_win, 6, 38, 3, 1));
 	set_menu_win(aperture_menu, aperture_win);
 	set_menu_sub(aperture_menu, derwin(aperture_win, 6, 38, 3, 1));
+	set_menu_format(exposure_menu, 5, 1);
 	set_menu_format(iso_menu, 5, 1);
 	set_menu_format(shutter_menu, 5, 1);
 	set_menu_format(aperture_menu, 5, 1);
 
 	/* Set menu mark to the string " * " */
+	set_menu_mark(exposure_menu, " * ");
 	set_menu_mark(iso_menu, " * ");
 	set_menu_mark(shutter_menu, " * ");
 	set_menu_mark(aperture_menu, " * ");
 
 	/* Print a border around the main window and print a title */
+	box(exposure_win, 0, 0);
 	box(iso_win, 0, 0);
 	box(shutter_win, 0, 0);
 	box(aperture_win, 0, 0);
+	print_in_middle(exposure_win, 1, 0, 40, "EV", COLOR_PAIR(1));
 	print_in_middle(iso_win, 1, 0, 40, "ISO", COLOR_PAIR(1));
 	print_in_middle(shutter_win, 1, 0, 40, "Shutter", COLOR_PAIR(1));
 	print_in_middle(aperture_win, 1, 0, 40, "Aperture", COLOR_PAIR(1));
+	mvwaddch(exposure_win, 2, 0, ACS_LTEE);
 	mvwaddch(iso_win, 2, 0, ACS_LTEE);
 	mvwaddch(shutter_win, 2, 0, ACS_LTEE);
 	mvwaddch(aperture_win, 2, 0, ACS_LTEE);
+	mvwhline(exposure_win, 2, 1, ACS_HLINE, 38);
 	mvwhline(iso_win, 2, 1, ACS_HLINE, 38);
 	mvwhline(shutter_win, 2, 1, ACS_HLINE, 38);
 	mvwhline(aperture_win, 2, 1, ACS_HLINE, 38);
+	mvwaddch(exposure_win, 2, 39, ACS_RTEE);
 	mvwaddch(iso_win, 2, 39, ACS_RTEE);
 	mvwaddch(shutter_win, 2, 39, ACS_RTEE);
 	mvwaddch(aperture_win, 2, 39, ACS_RTEE);
 
 	/* Post the menu */
+	post_menu(exposure_menu);
 	post_menu(iso_menu);
 	post_menu(shutter_menu);
 	post_menu(aperture_menu);
+	wrefresh(exposure_win);
 	wrefresh(iso_win);
 	wrefresh(shutter_win);
 	wrefresh(aperture_win);
@@ -287,10 +341,16 @@ int main() {
 		wrefresh(*win);
 	}	
 	/* Unpost and free all the memory taken up */
+	unpost_menu(expsoure_menu);
 	unpost_menu(iso_menu);
 	unpost_menu(shutter_menu);
 	unpost_menu(aperture_menu);
+	free_menu(exposure_menu);
 	free_menu(iso_menu);
+	free_menu(shutter_menu);
+	free_menu(aperture_menu);
+	for(i = 0; i < n_exposure; ++i)
+		free_item(iso_items[i]);
 	for(i = 0; i < n_iso; ++i)
 		free_item(iso_items[i]);
 	for(i = 0; i < n_shutter; ++i)
